6a784277ea7a617e7dbb52ee4a80e5c5b44b3287
hyeonseo <lindacoco0204@gmail.com>
2020-12-17 18:33:14 -0800

    changed function to class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import { render } from 'react-dom';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>management system</h2>
-        
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="gray-background">
+        <img src={logo} lat="logo" />
+        <h2>management system</h2>
+          
+      </div>
+    );
+  }
 }
 
-export default App;
